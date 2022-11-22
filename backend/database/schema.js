b1ae33aef5d666b65d195729e9dbed720065eae1
Махаличев Никита <54910038+mahalichev@users.noreply.github.com>
2022-11-22 14:23:18 +0300

    Homework collection + removing sub-ids

--- backend/database/schema.js
+++ backend/database/schema.js
@@ -2,52 +2,59 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+const Attempt = new Schema ({
+    start_timestamp: {type: Date, default: Date.now},
+    end_timestamp  : {type: Date},
+    user_answer    : {type: Number},
+    status         : {type: String}
+}, {_id: false, versionKey: false})
+
+const Task = new Schema ({
+    content         : {type: String},
+    create_timestamp: {type: Date, default: Date.now},
+    categories      : {type: [String]},
+    correct_answer  : {type: Number},
+    attempts        : {type: [Attempt]}
+}, {_id: false, versionKey: false})
+
 const User = new Schema ({
     email     : {type: String},
     password  : {type: String},
     first_name: {type: String},
     last_name : {type: String},
     role      : {type: String},
-    classes     : {type: [Schema.Types.ObjectId], ref: 'Class'},
-    tasks     : {type: [{
-        content         : {type: String},
-        create_timestamp: {type: Date, default: Date.now},
-        categories      : {type: [String]},
-        correct_answer  : {type: Number},
-        attempts        : {type: [{
-            start_timestamp: {type: Date, default: Date.now},
-            end_timestamp  : {type: Date},
-            user_answer    : {type: Number},
-            status         : {type: String}
-        }]}
-    }]},
+    classes   : {type: [Schema.Types.ObjectId], ref: 'Class'},
+    tasks     : {type: [Task]},
     logs      : {type: [Schema.Types.ObjectId], ref: 'Log'}
-});
+}, {versionKey: false});
+
+const HomeworkTask = new Schema ({
+    categories: {type: [String]},
+    count     : {type: Number}
+}, {_id: false, versionKey: false})
+
+const Homework = new Schema ({
+    created_timestamp : {type: Date, default: Date.now},
+    deadline_timestamp: {type: Date},
+    tasks             : {type: [HomeworkTask]}
+}, {versionKey: false})
 
 const Class = new Schema ({
     title    : {type: String},
     teacher  : {type: Schema.Types.ObjectId, ref: 'User'},
     pupils   : {type: [Schema.Types.ObjectId], ref: 'User'},
-    homeworks: {type: [{
-        created_timestamp : {type: Date, default: Date.now},
-        deadline_timestamp: {type: Date},
-        tasks             : {type: [{
-            categories: {type: [String]},
-            count     : {type: Number}
-        }]}
-    }]}  
-});
+    homeworks: {type: [Schema.Types.ObjectId], ref: 'Homework'}
+}, {versionKey: false});
 
 const Log = new Schema ({
     user_id  : {type: Schema.Types.ObjectId, ref: 'User'},
     timestamp: {type: Date, default: Date.now},
     level    : {type: String},
     content  : {type: String}
-})
+}, {versionKey: false})
 
 
-
-module.exports.users   = mongoose.model('Users', User)
-module.exports.classes = mongoose.model('Classes', Class)
-module.exports.logs    = mongoose.model('Logs', Log)
- 
+module.exports.users     = mongoose.model('Users', User)
+module.exports.classes   = mongoose.model('Classes', Class)
+module.exports.homeworks = mongoose.model('Homeworks', Homework)
+module.exports.logs      = mongoose.model('Logs', Log)
--- backend/routers/router.js
+++ backend/routers/router.js
@@ -4,17 +4,7 @@ const ObjectId = require('mongoose').Types.ObjectId
 const schema = require('../database/schema')
 
 router.get('/', async (req,res) => {
-	/*const user = new schema.users({
-		email: 'ilovemath@gmail.com',
-		password: 'mathematic_',
-		first_name: 'Владислав',
-		last_name: 'Думский',
-		role: 'teacher',
-		class: new ObjectId()
-	})
-	*/
-	//await user.save()
-	//console.log(JSON.stringify(result))
+	
 	res.json({ first: "Nikita" ,second :"Miha", third:"Max"})
 });
 
@@ -61,11 +51,28 @@ const user = new schema.users({
 await user.save()
 */
 
+/*
+const class_ = new schema.classes({
+	title    : '3A',
+	teacher  : ObjectId('637c0155e1c4c441e229e1e9'),
+	pupils   : [ObjectId('637c00d4f60019eba4826203'), ObjectId('637c00f9f08dc7e8cbb59d83'), ObjectId('637c989b3922c3052c79a92a')]
+})
+await class_.save()
+*/
+
 /* 1. Восстановление задачи при авторизации
-const userid = ObjectId('637be1b2db9fc12900889c67')
-const result = await schema.users.findOne({'user_id': userid, 'tasks.attempts.status': 'in progress'},
+const user_id = ObjectId('637be1b2db9fc12900889c67')
+const result = await schema.users.findOne({'_id': user_id, 'tasks.attempts.status': 'in progress'},
 										  {'_id': 0, 'tasks': {$elemMatch: {'attempts.status': 'in progress'}}})
-console.log(JSON.stringify(result))
+
+*/
+
+/* 4. Получение всех учеников класса
+const class_id = ObjectId('637c02395d814ec03b85cf56')
+const responce = await schema.classes.findOne({'_id': class_id}, {'_id': 0, 'pupils': 1})
+pupil_ids = responce.pupils
+const result = await schema.users.find({'_id': {$in: pupil_ids}})
+
 */
 
 /* 7. Создание нового пользователя
@@ -83,4 +90,15 @@ await user.save()
 const email = 'mahalichev.n@gmail.com'
 const result = await schema.users.findOne({'email': email})
 console.log(JSON.stringify(result))
+*/
+
+/* 9. Добавление ученика в класс
+user_id = ObjectId('637c005c70155b1e5f0ddc25')
+class_id = ObjectId('637c02395d814ec03b85cf56')
+result = await schema.users.find({'_id': user_id}, {'_id': 0, 'classes': 1})
+user_current_class = result.classes
+await schema.users.updateOne({'_id': user_id}, {'classes': [class_id]})
+if (user_current_class != undefined)
+	await schema.classes.updateOne({'_id': user_current_class[0]}, {$pull: {'pupils': user_id}})
+await schema.classes.updateOne({'_id': class_id}, {$push: {'pupils': user_id}})
 */
