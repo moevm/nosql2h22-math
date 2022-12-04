ce555f1698121624f438490f1b6cb3ad11ee2e52
Михаил Эйсвальд <e_misha_vl@mail.ru>
2022-12-05 01:24:47 +0300

    API suggestion

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -3,8 +3,255 @@ const router  = express.Router();
 const ObjectId = require('mongoose').Types.ObjectId
 const schema = require('../database/schema')
 
+// export database
 router.get('/', async (req,res) => {
-	res.json({})
+	res.json({databaseContent: "comes here"});
+});
+
+// import database (replace existing)
+router.put("/", async (req, res) => {
+	const databaseContent = req.body.databaseContent;
+	res.send("Ok");
+});
+
+// submit attempt, return verdict (ok/not ok)
+router.post("/submit", async (req, res) => {
+	const taskId = req.body.taskId;
+	// const userId = req.session.userId;
+	const answer = req.body.answer;
+	const startTime = req.body.startTime;
+	const endTime = req.body.endTime;
+	// res.status(400).send("taskId is not ObjectId hex string")
+	// res.status(403).send("Task does not belong to a user")
+	res.send("correct")
+	// res.send("not correct")
+});
+
+// get all active homeworks and their progress for pupil
+// [not needed yet]
+
+// get task by categories; if unsolved task exists, return it instead of creating a new task
+router.get("/task", async (req, res) => {
+	// const userId = req.session.userId;
+	const categories = req.query.categories.split("+");
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(401).send("Unauthorized")
+	res.json({task: "goes here"});
+});
+
+// get categories from last task pupil tried to solve before
+// [not needed yet]
+
+// get attempts for a user filtered by...
+router.get("/personal/attempts",
+	async (req, res) => {
+		// const userId = req.session.userId;
+		const sortByDatetime = req.query.sortByDatetime; // "asc" | "desc" | null
+		const sortBySolvingTime = req.query.sortBySolvingTime; // "asc" | "desc" | null
+		const taskContent = req.query.taskContent;
+		const categories = req.query.categories;
+		const answer = req.query.answer;
+		const verdict = req.query.verdict; // null | "correct" | "not correct" | "in progress"
+		// res.status(400).send(`${something} is not ${some_type}`)
+		// res.status(401).send("Unauthorized")
+		res.json({
+			attempts: [
+				{
+					datetime: "2022-11-15T06:31:15.000Z",
+					taskContent: "16x3-17",
+					categories: ["subtraction", "multiplication"],
+					solvingTime: "1970-01-01T00:01:02.000Z",
+					answer: 42,
+					verdict: false
+				},
+				{
+					datetime: "2022-11-15T09:25:00.000Z",
+					taskContent: "82-65-11",
+					categories: ["subtraction"],
+					solvingTime: "1970-01-01T00:00:42.000Z",
+					answer: 6,
+					verdict: true
+				}
+			],
+			correct: 1,
+			wrong: 1
+		});
+	});
+
+// publish new homework
+router.post("/classes/homeworks", async (req, res) => {
+	const classIds = JSON.parse(req.body.classIds);
+	const deadline = new Date(req.body.deadline);
+	const homeworkTasks = JSON.parse(req.body.homeworkTasks);
+	// const userId = req.session.userId;
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Forbidden")
+	res.status(201).send("Created");
+});
+
+// add a class
+router.post("/classes", async (req, res) => {
+	const className = req.body.className;
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(401).send("Unauthorized")
+	// res.status(409).send(`Class named "${className}" already exists`)
+	res.status(201).json({classId: "goes here"});
+});
+
+// add a pupil to class
+router.post("/classes/:id([0-9a-f]+)/join", async (req, res) => {
+	const classId = req.params.id;
+	// const userId = req.session.userId;
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Only a pupil can join a class")
+	// res.status(409).send("Pupil already present in a class")
+	res.send("Ok");
+});
+
+// get statistics for all students in a class (Class Page)
+router.get("/classes/:id([0-9a-f]+)/stats", async (req, res) => {
+	const classId = req.params.id;
+	// const userId = req.session.userId;
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Forbidden")
+	res.json({stats: "go here"})
+	/*[
+	{
+		firstName: "Fedor",
+		lastName: "Fedorov",
+		email: "fedorov.f@example.com",
+		lastActivity: "2022-11-15T06:31:15.000Z",
+		homeworkProgress: 6,
+		errorCount: {
+			addition: 1,
+			subtraction: 8,
+			multiplication: 2,
+			division: 38
+		},
+		lastDistractionTime: "2022-11-01T09:58:15.000Z",
+		lastDistractionSolveTime: "1970-01-01T00:05:40.000Z"
+	}, ... ]
+	 */
+});
+
+// get aggregated statistics for all classes by teacher (Add Class Process Page)
+router.get("/classes", async (req, res) => {
+	// const userId = req.session.userId;
+	// res.status(401).send("Log in to proceed")
+	res.json({data: "goes here"});
+	/*
+	[
+		{
+			name: "1A",
+			pupilCount: 24,
+			homeworks: [...],
+			doneLastHomework: 22,
+			totalSubmissions: 576,
+			correctSubmissions: 384
+		},
+	...]
+	 */
+});
+
+// delete a class
+router.delete("/classes/:id([0-9a-f]+)", async (req, res) => {
+	const classId = req.params.id;
+	// const userId = req.session.userId;
+	// res.status(400).send("Class id is not an ObjectId hex string")
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Teacher must run a class to delete it")
+	// res.status(404).send("Class not found")
+	res.json("Ok");
+});
+
+// register
+router.post("/register", async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const role = req.body.role; // "teacher" | "pupil"
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(409).send(`User with email '${email}' already exists`)
+	res.status(201).send("created");
+});
+
+// log in
+router.post("/login", async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	// res.status(403).send("Wrong credentials")
+	// res.cookie("userId", "982d5a002e458358a5df6012"); res.send("Ok");
+});
+
+// get history by...
+router.get("/history", async (req, res) => {
+	const emailText = req.query.email;
+	const userId = req.query.userId;
+	const sortByDateTime = req.query.sortByDateTime; // null | "asc" | "desc"
+	const role = req.query.role; // null | "pupil" | "teacher"
+	const actionText = req.query.action;
+	const messageText = req.query.message;
+	// const adminId = req.session.userId;
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Forbidden")
+	res.json({history: "comes here"});
+	/*
+	[
+		{
+			datetime: "2022-11-15T06:31:15.000Z",
+			email: "fedorov.f@example.com",
+			role: "pupil",
+			action: "Вход",
+			message: "..."
+		},
+		...
+	]
+	 */
+});
+
+// get logs by...
+router.get("/logs", async (req, res) => {
+	// const adminId = req.session.userId;
+	const sortByDateTime = req.query.sortByDateTime; // null | "asc" | "desc"
+	const logLevels = req.query.logLevels;
+	const messageText = req.query.message;
+	// res.status(400).send(`${something} is not ${some_type}`)
+	// res.status(401).send("Log in to proceed")
+	// res.status(403).send("Forbidden")
+	res.json({log: "comes here"});
+	/*
+	[
+		{
+			datetime: "2022-11-15T06:31:15.000Z",
+			level: "DEBUG",
+			message: "..."
+		},
+		...
+	]
+	 */
+});
+
+// get attempts by class & filter by...
+// [not needed yet]
+
+// fallback
+router.get("*", (req, res) => {
+	res.status(404).send("No valid endpoint for that")
+});
+
+router.post("*", (req, res) => {
+	res.status(404).send("No valid endpoint for that")
+});
+
+router.put("*", (req, res) => {
+	res.status(404).send("No valid endpoint for that")
+});
+
+router.delete("*", (req, res) => {
+	res.status(404).send("No valid endpoint for that")
 });
 
 module.exports = router;
