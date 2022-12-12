fd3fa05067ec75505de2059c633eb9dbaf0ee614
Махаличев Никита <54910038+mahalichev@users.noreply.github.com>
2022-12-13 00:39:19 +0300

    Frontend Updates

--- frontend/src/pages/components/Navigation.jsx
+++ frontend/src/pages/components/Navigation.jsx
@@ -1,117 +1,141 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
-import styles from '../styles/Navigation.module.css'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {NavLink, useNavigate} from 'react-router-dom';
+import styles from '../styles/Navigation.module.css';
 
 
-export default function Navigation() {
-    let activeStyle = {
+export default function Navigation({navTrigger, setNavTrigger}) {
+
+    const instance = axios.create({
+        baseURL: "http://localhost:8000",
+        withCredentials: true
+    })
+
+    const navigate = useNavigate();
+
+    const activeStyle = {
         color: "#07889B"
     };
+
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName: "",
+        role: ""
+    });
+
+    useEffect(() => {instance.get("/whoami")
+                             .then(res => (res.data != null) ?
+                                          setUser({firstName: res.data.first_name,
+                                                   lastName: res.data.last_name,
+                                                   role: res.data.role}) :
+                                          null)}, [navTrigger]);
     
-    let navigation = <></>
-    let user = "administrator"
-    switch(user){
-        case "pupil":
-            navigation = <div className={styles.navbar}>
-                <div className={styles.center}>
-                    <NavLink
-                        to="/"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Задачи</NavLink>
-                    <NavLink
-                        to="/stats"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Статистика</NavLink>
-                    <NavLink
-                        to="/user_history"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>История</NavLink>
+    const logoutUser = async() => {
+        await instance.get("/logout");
+        setUser({firstName: "",
+                 lastName: "",
+                 role: ""});
+        navigate("/login");
+    };
+
+    const getNavigation = () => {
+        var navigation = <></>;
+
+        switch(user.role){
+            case "pupil":
+                navigation = <div className={styles.navbar}>
+                    <div className={styles.center}>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Задачи</NavLink>
+                        <NavLink
+                            to="/stats"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Статистика</NavLink>
+                        <NavLink
+                            to="/user_history"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>История</NavLink>
+                    </div>
+                    <div className={styles.right}>
+                        <div>{user.firstName} {user.lastName}</div>
+                        <NavLink onClick={logoutUser}>Выйти</NavLink>
+                    </div>
                 </div>
-                <div className={styles.right}>
-                    <div>Имя Фамилия</div>
-                    <NavLink
-                        to="/logout"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Выйти</NavLink>
+                break;
+
+            case "teacher":
+                navigation = <div className={styles.navbar}>
+                    <div className={styles.center}>
+                        <NavLink
+                            to="/add_hometask"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Добавить задание</NavLink>
+                        <NavLink
+                            to="/classes"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Классы</NavLink>
+                    </div>
+                    <div className={styles.right}>
+                        <div>{user.firstName} {user.lastName}</div>
+                        <NavLink onClick={logoutUser}>Выйти</NavLink>
+                    </div>
                 </div>
-            </div>
-            break;
-        case "teacher":
-            navigation = <div className={styles.navbar}>
-                <div className={styles.center}>
-                    <NavLink
-                        to="/add_hometask"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Добавить задание</NavLink>
-                    <NavLink
-                        to="/classes"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Классы</NavLink>
+                break;
+            
+            case "administrator":
+                navigation = <div className={styles.navbar}>
+                    <div className={styles.center}>
+                        <NavLink
+                            to="/logs"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Логи</NavLink>
+                        <NavLink
+                            to="/history"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>История действий</NavLink>
+                    </div>
+                    <div className={styles.right}>
+                        <div>Administrator</div>
+                        <NavLink onClick={logoutUser}>Выйти</NavLink>
+                    </div>
                 </div>
-                <div className={styles.right}>
-                    <div>Имя Фамилия</div>
-                    <NavLink
-                        to="/logout"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Выйти</NavLink>
+                break;
+
+            default:
+                navigation = <div className={styles.navbar}>
+                    <div className={styles.center}>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Задачи</NavLink>
+                        <NavLink
+                            to="/login"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Войти</NavLink>
+                        <NavLink
+                            to="/register"
+                            style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                            }>Регистрация</NavLink>
+                    </div>
                 </div>
-            </div>
-            break;
-        case "administrator":
-            navigation = <div className={styles.navbar}>
-                <div className={styles.center}>
-                    <NavLink
-                        to="/logs"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Логи</NavLink>
-                    <NavLink
-                        to="/history"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>История действий</NavLink>
-                </div>
-                <div className={styles.right}>
-                    <div>Administrator</div>
-                    <NavLink
-                        to="/logout"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Выйти</NavLink>
-                </div>
-            </div>
-            break;
-        default:
-            navigation = <div className={styles.navbar}>
-                <div className={styles.center}>
-                    <NavLink
-                        to="/"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Задачи</NavLink>
-                    <NavLink
-                        to="/login"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Войти</NavLink>
-                    <NavLink
-                        to="/register"
-                        style={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                        }>Регистрация</NavLink>
-                </div>
-            </div>
-            break;
+                break;
+        }
+        return navigation;
     }
 
     return (
-        navigation
+        getNavigation()
     )
 }
--- frontend/src/pages/components/Task.jsx
+++ frontend/src/pages/components/Task.jsx
@@ -1,20 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import {Switch, Space} from 'antd'
-import {IconButton, TextField} from '@mui/material'
-import {CancelOutlined} from '@mui/icons-material'
-import styles from '../styles/Task.module.css'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {FormControlLabel, IconButton, TextField, Switch} from '@mui/material';
+import {CancelOutlined, ArrowForwardRounded} from '@mui/icons-material';
+import styles from '../styles/Task.module.css';
 
 export default function Task(){
-    const [values, setValues] = React.useState({
-        input: "",
-        task: "23-5x4",
-        expected: "3",
+    const instance = axios.create({
+        baseURL: 'http://localhost:8000',
+        withCredentials: true
+    });
+
+    const [categories, setCategories] = useState({
+        addition: true,
+        subtraction: true,
+        multiplication: true,
+        division: true
+    });
+
+    const [task, setTask] = useState({
+        id: "",
+        content: "",
+        user_answer: "",
+        correct_answer: "",
+        was_resolved: false,
         input_color: "primary"
     });
 
-    let role = 'pupil'
+    var role = "pupil";
 
-    let current_homeworks_response = [
+    var current_homeworks_response = [
         {deadline_timestamp: "11 января 2023",
          tasks: [{categories: ['сложение', 'умножение'],
                   count: 10,
@@ -36,11 +50,13 @@ export default function Task(){
          tasks: [{categories: ['сложение', 'вычитание', 'умножение', 'деление'],
                   count: 10,
                   progress: 5}]}
-    ]
-    let i = 0
-    let homeworks = <></>
+    ];
 
-    if (role === 'pupil')
+    var i = 0;
+
+    var homeworks = <></>;
+
+    if (role === "pupil")
         homeworks = <div className={styles.homeworks}>
                         {current_homeworks_response.map(homework => (
                         <div key={i++} className={styles.homework}>
@@ -50,82 +66,120 @@ export default function Task(){
                             </div>
                             <ol>
                                 {homework.tasks.map(task =>
-                                    <li key={i++}>{task.categories.join(' и ') + ' - ' + task.progress + '/' + task.count}</li>
+                                    <li key={i++}>{task.categories.join(" и ") + " - " + task.progress + "/" + task.count}</li>
                                 )}
                             </ol>
                         </div>
                         ))}
                     </div>
     
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value, input_color: "primary" });
+    const categoriesToArray = () => {
+        var result = [];
+        Object.keys(categories).forEach(category => {categories[category] ? result.push(category) : null});
+        return result;
     };
 
-    const handleClickClear = (prop) => (event) => {
-        setValues({ ...values, [prop]: "", input_color: "primary"});
+    const handleChangeAnswer = (event) => {
+        setTask({ ...task, user_answer: event.target.value});
     };
 
-    const handleSubmit = () => {
-        if (values.input == values.expected)
-            setValues({ ...values, input_color: "success" })
-        else
-            setValues({ ...values, input_color: "error" })
+    const handleClickClear = (event) => {
+        setTask({ ...task, user_answer: "", input_color: "primary"});
+    };
+
+    const handleChangeCategory = (e) => {
+        var category = e.target.value;
+        var newCategories = {...categories};
+        newCategories[category] = !newCategories[category];
+        if (Object.values(newCategories).some(val => val))
+            setCategories(newCategories);
+    };
+
+    useEffect(() => {instance.get(`/task?categories=${categoriesToArray().join(" ")}`)
+                             .then(res => setTask({...task,
+                                                   id: res.data._id,
+                                                   content: res.data.content,
+                                                   correct_answer: String(res.data.correct_answer),
+                                                   was_resolved: false,
+                                                   input_color: "primary"}))}, [categories]);
+
+    const handleSubmit = async () => {
+        if (task.was_resolved){
+            (task.user_answer === task.correct_answer) ? setTask({ ...task, input_color: "success"}) :
+                                                         setTask({ ...task, input_color: "error"});
+        } else {
+            var body = {
+                taskId: task.id,
+                answer: task.user_answer,
+                task: {
+                    correctAnswer: task.correct_answer
+                }
+            };
+
+            const response = await instance.post('/submit', body);
+
+            if (response.data.verdict === "correct"){
+                setTask({ ...task, input_color: "success", was_resolved: true});
+            } else setTask({ ...task, input_color: "error"});
+        }
     }
 
+    const moveForward = () => {
+        instance.get(`/task?categories=${categoriesToArray().join(" ")}`)
+                .then(res => setTask({...task,
+                                      id: res.data._id,
+                                      content: res.data.content,
+                                      correct_answer: String(res.data.correct_answer),
+                                      was_resolved: false,
+                                      input_color: "primary"}));
+        setTask({ ...task, user_answer: ""});
+    };
+
     const handleKeypress = e => {
-        if (e.key === 'Enter') handleSubmit()
-        if (e.key === 'Backspace') setValues({ ...values, input_color: "primary" });
+        if (e.key == "Enter") handleSubmit();
+        else setTask({ ...task, input_color: "primary"});
     };
 
     return (
         <>
             <div className={styles.main}>
                 <div className={styles.task}>
-                    <div className={styles.task_content}>{values.task}</div>
+                    <div className={styles.task_content}>{task.content}</div>
+                    <div className={styles.task_input}>
                         <TextField style={{width: 360, height: 60}}
-                                   id="input"
+                                   id="user_answer"
                                    label="Введите ответ"
                                    placeholder="Нажмите Enter для отправки"
                                    type="text"
-                                   value={values.input}
-                                   onChange={handleChange("input")}
+                                   value={task.user_answer}
+                                   onChange={handleChangeAnswer}
                                    onKeyPress={handleKeypress}
-                                   color={values.input_color}
+                                   color={task.input_color}
                                    InputProps={{
                                         endAdornment: <IconButton key="input-clear-button"
-                                                                  onClick={handleClickClear("input")}
+                                                                  onClick={handleClickClear}
                                                                   edge="end">
                                                                   <CancelOutlined />
                                                       </IconButton>
                         }}/>
+                        {task.was_resolved ? <IconButton onClick={moveForward}><ArrowForwardRounded /></IconButton> : <></>}
+                    </div>
                 </div>
                 {homeworks}
             </div>
             
-            <div className={styles.task_categories}>
+            <div className={styles.categories}>
                 <div className={styles.switch_field}>
-                    <div className={styles.switch_content}>
-                        <Switch className={styles.switch_task} defaultChecked />
-                        <Space size={'large'}/> Сложение
-                    </div>
+                    <FormControlLabel className={styles.category} control={<Switch checked={categories.addition} onClick={handleChangeCategory} />} value="addition" label="Сложение" labelPlacement="end" />
                 </div>
                 <div className={styles.switch_field}>
-                    <div className={styles.switch_content}>
-                        <Switch className={styles.switch_task} defaultChecked />
-                        <Space /> Вычитание
-                    </div>
+                    <FormControlLabel className={styles.category} control={<Switch checked={categories.subtraction} onClick={handleChangeCategory} />} value="subtraction" label="Вычитание" labelPlacement="end" />
                 </div>
                 <div className={styles.switch_field}>
-                    <div className={styles.switch_content}>
-                        <Switch className={styles.switch_task} defaultChecked />
-                        <Space /> Умножение
-                    </div>
+                    <FormControlLabel className={styles.category} control={<Switch checked={categories.multiplication} onClick={handleChangeCategory} />} value="multiplication" label="Умножение" labelPlacement="end" />
                 </div>
                 <div className={styles.switch_field}>
-                    <div className={styles.switch_content}>
-                        <Switch className={styles.switch_task} defaultChecked />
-                        <Space /> Деление
-                    </div>
+                    <FormControlLabel className={styles.category} control={<Switch checked={categories.division} onClick={handleChangeCategory} />} value="division" label="Деление" labelPlacement="end" />
                 </div>
             </div>
         </>
