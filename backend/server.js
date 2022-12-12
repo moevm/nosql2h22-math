a2c97344ba91306d49a17345c92974d7e72bc6e0
Махаличев Никита <54910038+mahalichev@users.noreply.github.com>
2022-12-13 02:12:01 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,28 +35,6 @@ app.use(cookieParser("secret00"));
 app.use(session({secret: "secret00"}));
 app.use(cors(corsOptions));
 
-// Add headers before the routes are defined
-/*app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    res.setHeader('Content-Security-Policy', "default-src 'self'")
-  
-    // Pass to next layer of middleware
-    next();
-}); */
-
 app.use("/", routes);
 
 
